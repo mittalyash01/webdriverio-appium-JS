b5aa685a2f6d0dd7bc61fddbfecba9732ee74dcd
Mittal <yashshvi.a.mittal@accenture.com>
2023-03-09 13:26:23 -0500

    formatted

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,11 +1,11 @@
 const LoginScreen = require("../screenobjects/login.screen");
 const ProductListScreen = require("../screenobjects/product-list.screen");
 const SidePanelScreen = require("../screenobjects/side-panel.screen");
-const ProductOnesieScreen= require("../screenobjects/product-onesie.screen");
+const ProductOnesieScreen = require("../screenobjects/product-onesie.screen");
 
 describe('Login', () => {
 
-  afterEach(async function() {
+  afterEach(async function () {
     await ProductListScreen.menuButton.click();
     await SidePanelScreen.logOutButton.click();
     await expect(LoginScreen.usernameField && LoginScreen.passwordField).toBeDisplayed();
@@ -37,37 +37,37 @@ describe('Login', () => {
     await LoginScreen.passwordField.setValue("secret_sauce");
     await LoginScreen.loginButton.click();
     await expect(ProductListScreen.productList).toBeDisplayed();
- 
-  const size = await driver.getWindowRect();
-  console.log(`SCREEN SIZE: ${size.width}x${size.height}`);
-  start_y= size['height']*0.8
-  start_x=size['width']*0.5
-  end_x= size['width']*0.5
-  end_y= size['height']*0.1
 
-  await driver.performActions([
-    {
+    const size = await driver.getWindowRect();
+    console.log(`SCREEN SIZE: ${size.width}x${size.height}`);
+    start_y = size['height'] * 0.8
+    start_x = size['width'] * 0.5
+    end_x = size['width'] * 0.5
+    end_y = size['height'] * 0.1
+
+    await driver.performActions([
+      {
         // a. Create the event                    
         type: 'pointer',
         id: 'finger1',
         parameters: { pointerType: 'touch' },
         actions: [
-            // b. Move finger into start position                        
-            { type: 'pointerMove', duration: 0, x: start_x, y: start_y },
-            // c. Finger comes down into contact with screen                        
-            { type: 'pointerDown', button: 0 },
-            // d. Pause for a little bit                        
-            { type: 'pause', duration: 100 },
-            // e. Finger moves to end position                        
-            //    We move our finger from the center of the element to the                        
-            //    starting position of the element.                        
-            //    Play with the duration to make the swipe go slower / faster                        
-            { type: 'pointerMove', duration: 1000, x: end_x, y: end_y },
-            // f. Finger gets up, off the screen                        
-            { type: 'pointerUp', button: 0 },
+          // b. Move finger into start position                        
+          { type: 'pointerMove', duration: 0, x: start_x, y: start_y },
+          // c. Finger comes down into contact with screen                        
+          { type: 'pointerDown', button: 0 },
+          // d. Pause for a little bit                        
+          { type: 'pause', duration: 100 },
+          // e. Finger moves to end position                        
+          //    We move our finger from the center of the element to the                        
+          //    starting position of the element.                        
+          //    Play with the duration to make the swipe go slower / faster                        
+          { type: 'pointerMove', duration: 1000, x: end_x, y: end_y },
+          // f. Finger gets up, off the screen                        
+          { type: 'pointerUp', button: 0 },
         ],
-    },
-]);
+      },
+    ]);
 
     await expect(ProductListScreen.onesieShirt).toBeDisplayed();
     await ProductListScreen.onesieShirt.click();
