469db595b38272945bfa52b7c7177f3788c88a32
RamyaVMurugan <ramyasumothy@gmail.com>
2023-03-13 01:23:32 -0400

    Test case for invalid credentials
    Added

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -24,4 +24,14 @@ describe('Login', () => {
     await expect(LoginScreen.errorMessage).toHaveText('Sorry, this user has been locked out.');
     await new Promise(f => setTimeout(f, 2000));
   });
+
+  it('LOGIN_invalid_user', async () => {
+    await LoginScreen.usernameField.setValue("invalid_username");
+    await LoginScreen.passwordField.setValue("invalid_password");
+    await LoginScreen.loginButton.click();
+    await expect(LoginScreen.errorMessage).toBeDisplayed();
+    await expect(LoginScreen.errorMessage).toHaveText('Username and password do not match any user in this service.');
+    await new Promise(f => setTimeout(f, 2000));
+  });
+
 });
