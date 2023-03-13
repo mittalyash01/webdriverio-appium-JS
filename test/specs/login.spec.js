197e3e7e01407e4c25c35c16d9b4945ad861bdd0
RamyaVMurugan <ramyasumothy@gmail.com>
2023-03-13 01:35:11 -0400

    Edited

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -3,7 +3,7 @@ const ProductListScreen = require("../screenobjects/product-list.screen");
 const SidePanelScreen = require("../screenobjects/side-panel.screen");
 
 describe('Login', () => {
-
+/*
   it('LOGIN_valid_user', async () => {
     await LoginScreen.usernameField.setValue("standard_user");
     await LoginScreen.passwordField.setValue("secret_sauce");
@@ -24,7 +24,7 @@ describe('Login', () => {
     await expect(LoginScreen.errorMessage).toHaveText('Sorry, this user has been locked out.');
     await new Promise(f => setTimeout(f, 2000));
   });
-
+*/
   it('LOGIN_invalid_user', async () => {
     await LoginScreen.usernameField.setValue("invalid_username");
     await LoginScreen.passwordField.setValue("invalid_password");
