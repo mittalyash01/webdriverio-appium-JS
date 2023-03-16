16446ad5850d7ede910037745689fda25693586f
kollonqv <ollonqvistkalle@gmail.com>
2023-03-15 21:40:05 -0700

    remove everything from gh-pages except docs and allure

--- test/screenobjects/product-onesie.screen.js
+++
@@ -1,8 +0,0 @@
-
-class ProductOnesieScreen {
-  get onesieTitle() {
-    return $('//*[@content-desc="Sauce Labs Onesie" or @name="Sauce Labs Onesie"]');
-  }
-}
-
-module.exports = new ProductOnesieScreen();
--- test/screenobjects/side-panel.screen.js
+++
@@ -1,8 +0,0 @@
-
-class SidePanelScreen {
-  get logOutButton() {
-    return $('//*[@content-desc="test-LOGOUT" or @name="test-LOGOUT"]');
-  }
-}
-
-module.exports = new SidePanelScreen();
