de0e2928f655523224574272a4e78a69016e9c78
Kalle <ollonqvistkalle@gmail.com>
2023-03-21 14:16:15 -0700

    Add parameters to browserstack CI job (#14)
    Update browserstack.android.conf.js

--- config/browserstack/browserstack.ios.conf.js
+++ config/browserstack/browserstack.ios.conf.js
@@ -20,8 +20,8 @@ config.capabilities = [
     user: process.env.BROWSERSTACK_USERNAME || user,
     key: process.env.BROWSERSTACK_ACCESS_KEY || key,
     "platformName" : "ios",
-    "appium:platformVersion" : "16.0",
-    "appium:deviceName" : "iPhone 14",
+    "appium:platformVersion" : process.env.npm_config_osversion || "16.0",
+    "appium:deviceName" : process.env.npm_config_device || "iPhone 14",
     "appium:app" : "bs://e5bb603500754df9402c2bcd7db4f5bd7d79a81b",
   }
 ]
