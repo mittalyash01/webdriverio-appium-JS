6226f3e9bd0c3ba8b61903c024858d63c4a6e8d9
kollonqv <ollonqvistkalle@gmail.com>
2023-03-21 14:01:16 -0700

    Add parameters to browserstack CI job
    Update browserstack.android.conf.js

--- config/browserstack/browserstack.android.conf.js
+++ config/browserstack/browserstack.android.conf.js
@@ -20,8 +20,8 @@ config.capabilities = [
     user: process.env.BROWSERSTACK_USERNAME || user,
     key: process.env.BROWSERSTACK_ACCESS_KEY || key,
     "platformName" : "android",
-    "appium:platformVersion" : "12.0",
-    "appium:deviceName" : "Samsung Galaxy S22 Ultra",
+    "appium:platformVersion" : process.env.npm_config_osversion || "12.0",
+    "appium:deviceName" : process.env.npm_config_device || "Samsung Galaxy S22 Ultra",
     "appium:app" : "bs://eeb58d587be0810d5de10ec396de1662142f7057",
   }
 ]
